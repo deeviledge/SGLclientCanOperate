c01064d2c8361e3b03349d93962ff58594dd1bcb
misimaLab <ai.mychronicle07@gmail.com>
2016-01-28 23:25:58 +0900

    動作確認済みのSGLClient
    小田さんの鍵共有プロトコル採用バージョン
    前のバージョンとはことなり小田さんの鍵配送指令書作成アルゴリズムを採用している
    それにより西村さんの代のSGLとは大きく異なる
    主に変更したのは
    ・鍵配送指令書作成プログラム周り
    ・DynamicPeerInfotmationの編集周り
    ・RoundMain周り
    ・鍵交換の周りなど
    
    鍵配送アルゴリズムは初期のバージョンは動かないので
    新しく作り直した
    
    ※注意
    西村バージョンとは異なるので変更したファイルだけを変えてもたぶん動かない
    このバージョンで動かせばちゃんと動く

---
+++ src/sglclient/keyexchange/SendFrom.java
@@ -0,0 +1,97 @@
+package sglclient.keyexchange;
+/**
+ * 自分が先に鍵を送る時呼び出す.
+ * 公開鍵の交換を行うクラス.
+ * 
+ * @author nishimura
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.math.BigInteger;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class SendFrom {
+	private Socket socket;
+        private ServerSocket serversoc;
+	public  SendFrom(String peerip,int roundport){
+		try {
+			socket = new Socket( peerip , roundport );
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+	}
+        
+        //コンストラクタのオーバーライド
+        public  SendFrom(String peerip,String serverip,int roundport){
+            /*
+		try {
+                    System.out.println("ソケットを生成/ServerSocketに接続を要求します");
+                    System.out.println(serverip+":"+roundport);
+                    socket = new Socket( serverip , roundport );
+                    
+                    System.out.println("SGLサーバ："+socket.getInetAddress()+"との接続完了");//接続先アドレスを返して表示
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+                        System.out.println("ソケット生成プロセスでなんかあったぞ！");
+		}
+            */    
+		try {
+                    System.out.println("ソケットを生成/ServerSocketに接続を要求します");
+                    System.out.println(serverip+":"+roundport);
+                    serversoc = new ServerSocket(roundport);
+                    socket=serversoc.accept();
+                    socket.close();
+                    serversoc.close();
+                    
+                    System.out.println("SGLサーバ："+socket.getInetAddress()+"との接続完了");//接続先アドレスを返して表示
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+                        System.out.println("ソケット生成プロセスでなんかあったぞ！");
+		}
+                
+                
+                
+                
+                
+	}
+        
+        
+        
+	/**
+	 * 
+	 * @param pk
+	 * @return 受け取った公開鍵
+	 */
+	public String KeyExchange(BigInteger pk){
+		String line = null;
+		try {
+			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			//公開鍵を送信
+			String input = "pk:";
+			input += ""+pk;//文字列変換
+			out.println(input);
+			//System.out.println("Peer client :" + "送信:" + input);
+			//公開鍵を受信
+			line = in.readLine();
+			//System.out.println("Peer client :" + "受信:" + line);
+			socket.close();
+			in.close();
+			out.close();
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		
+		return line;
+		
+	}
+}
+
